eed2611ef04ae47e854772fa82f9628b854c3918
Luiz Marangoni <lzmarangoni@gmail.com>
2022-11-25 15:09:22 -0300

    gravando os valores da lista de posições dos jogadores em variaveis usando usestate

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -9,17 +9,29 @@ export const Form = (props)=>{
     const positions = ['goleiro', 'zagueiro'];
     function savePlayer(e){
         e.preventDefault();
-        console.log("formulario salbo")
+        console.log(InputPlayer,InputImage,playerList)
       }
     const [InputPlayer , setInputPlayer] = useState("Canarinho Pistola")
     const [InputImage, setInputImage] = useState("https...")
+    const [playerList, SetPlayerList] = useState("")
+   
     
     return(
         <div className="form-players">
             <form onSubmit={savePlayer} >
-                <Input value={InputPlayer} changeValue={setInputPlayer} label="Nome do Jogador"/>
-                <Input value={InputImage} changeValue={setInputImage} label="Foto"/>
-                <List listName="Posição" items= {positions} />
+                <Input 
+                    value={InputPlayer} 
+                    changeValue={setInputPlayer} 
+                    label="Nome do Jogador"/>
+                <Input 
+                    value={InputImage} 
+                    changeValue={setInputImage} 
+                    label="Foto"/>
+                <List 
+                    listName="Posição" 
+                    items= {positions}
+                    value = {playerList}
+                    changeValue = {playerList => SetPlayerList(playerList)} />
                 <Button nameButton="Escalar"/>
             </form>
         </div>
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,10 +1,14 @@
 import "./List.css"
 
 export const List =(props)=>{
+        const selectOption = (e)=>{
+            props.changeValue(e.target.value)
+            
+        }
     return(
         <div className="list">
             <label>{props.listName}</label>
-            <select>
+            <select onChange={selectOption} value={props.value}>
                 {props.items.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
